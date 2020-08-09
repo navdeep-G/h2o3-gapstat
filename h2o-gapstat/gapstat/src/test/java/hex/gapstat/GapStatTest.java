a25a32cdb9af1efa3916fc6ddf8638f0c5642594
navdeep-G <mr.navdeepgill@gmail.com>
2020-08-09 08:59:16 -0700

    Add KMeans for ref

--- h2o-gapstat/gapstat/src/test/java/hex/gapstat/GapStatTest.java
+++ h2o-gapstat/gapstat/src/test/java/hex/gapstat/GapStatTest.java
@@ -1,5 +1,7 @@
 package hex.gapstat;
 
+import hex.kmeans.KMeans;
+import hex.kmeans.KMeansModel;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import water.TestUtil;
@@ -25,6 +27,9 @@ public class GapStatTest extends TestUtil {
         Frame fr = null;
         String filePath;
         String target;
+
+        KMeansModel km = null;
+
         try {
             if (useIrisFromR) {
                 filePath = "smalldata/iris/iris_r.csv";
@@ -46,9 +51,28 @@ public class GapStatTest extends TestUtil {
             System.out.println("Best K: " + gs._output._best_k);
             System.out.println("Gap Statistics per cluster in range from 1 to `k_max`: " + Arrays.toString(gs._output._gap_stats));
 
+            KMeansModel.KMeansParameters parms_km = new KMeansModel.KMeansParameters();
+            parms_km._train = fr._key;
+            parms_km._seed = 1234;
+            parms_km._max_iterations = 50;
+            parms_km._k = 10;
+            parms_km._estimate_k = true;
+            parms_km._init = KMeans.Initialization.Furthest;
+
+            km = new KMeans(parms_km).trainModel().get();
+
+            // Look at clusters for Gap and KMeans
+            System.out.println("Number of clusters and: " + km._output._size.length);
+            System.out.println("Size of each cluster: " + Arrays.toString(km._output._size));
+            System.out.println("Best K: " + gs._output._best_k);
+            System.out.println("Gap Statistics per cluster in range from 1 to `k_max`: " + Arrays.toString(gs._output._gap_stats));
+
+
+
         } finally {
             if (fr != null) fr.delete();
             if (gs != null) gs.delete();
+            if (km != null) km.delete();
         }
     }
 }
